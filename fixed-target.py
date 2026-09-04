37fcc7e7f2a70393a5417a25dfa25b4eb3dcac3c
itsmhmiri <mhmiri.cs@gmail.com>
2026-09-04 20:46:18 +0330

    Implementing a simple 2d SAM simulation for intercepting fixed targets

---
+++ fixed-target.py
@@ -0,0 +1,45 @@
+import math
+
+class Target:
+    def __init__(self, x:float, y:float):
+        self.x = x
+        self.y = y
+
+
+class Missile:
+    def __init__(self, x:float, y:float, speed:float):
+        self.x = x
+        self.y = y
+        self.speed = speed
+
+    def distance(self, target:Target) -> float:
+        # calculating the distance between target and interceptor
+        return math.hypot((target.x -self.x), (target.y - self.y))
+
+    def step(self, target:Target, dt:float) -> float:
+        dx = target.x - self.x
+        dy = target.y - self.y
+        
+        # calculating line-of-sight (LOS)
+        los = math.atan2(dy, dx)
+
+        # heading toward the target with calculated LOS and speed
+        self.x += math.cos(los) * self.speed * dt
+        self.y += math.sin(los) * self.speed * dt
+
+        # calculating distance after moving
+        return self.distance(target)
+
+
+if __name__ == '__main__':
+    dt = 0.01
+    target = Target(300, 400)
+    missile = Missile(0, 0, 100)
+    hit_radius = 1.0
+    t = 0.0
+    d = missile.distance(target)
+    while d >= hit_radius:
+        d = missile.step(target, dt)
+        print(f"distance: {d:.4f}  |  step: {t:.4f}")
+        t += dt
+    print(f"Target got intercepted in {t//dt}")
