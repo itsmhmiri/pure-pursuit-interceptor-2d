54d70985de790878e7c1c4ddc7b804a4a3477801
itsmhmiri <mhmiri.cs@gmail.com>
2026-09-04 22:08:24 +0330

    Implementing a simple 2D SAM simulation for intercepting targets with the ability to do maneuvers mid-air (moving in a wave-like path) with a real turning angle limit on the interceptor

---
+++ maneuvering-target.py
@@ -0,0 +1,78 @@
+import math
+
+class ManeuveringTarget:
+    def __init__(self, x: float, y: float, base_vx: float, base_vy: float):
+        self.x = x
+        self.y = y
+        self.base_vx = base_vx
+        self.base_vy = base_vy
+        self.time = 0.0
+
+    def step(self, dt: float):
+        self.time += dt
+        vy_evasive = self.base_vy + 60.0 * math.sin(1.5 * self.time)
+        
+        self.x += self.base_vx * dt
+        self.y += vy_evasive * dt
+
+class ConstrainedMissile:
+    def __init__(self, x:float, y:float, speed:float, max_turn_deg:float = 30):
+        self.x = x
+        self.y = y
+        self.speed = speed
+        self.heading_deg = 0.0
+        self.max_turn_deg = math.radians(max_turn_deg)
+
+    def distance(self, target:MovingTarget) -> float:
+        # calculating the distance between target and interceptor
+        return math.hypot((target.x -self.x), (target.y - self.y))
+
+    def step(self, target:MovingTarget, dt:float) -> float:
+        dx = target.x - self.x
+        dy = target.y - self.y
+        
+        # calculating line-of-sight (LOS)
+        los = math.atan2(dy, dx)
+
+        # calculating angle difference between LOS and current angle (heading_deg)
+        angle_diff = (los - self.heading_deg + math.pi) % (2 * math.pi) - math.pi
+        
+        # calculating max possible turn in the current step
+        max_turn = self.max_turn_deg * dt
+
+        # capping the turning angle to maximum possible angle
+        clamped_turn = max(-max_turn, min(max_turn, angle_diff))
+        self.heading_deg += clamped_turn
+
+        # heading toward the target with calculated LOS and speed
+        self.x += math.cos(self.heading_deg) * self.speed * dt
+        self.y += math.sin(self.heading_deg) * self.speed * dt
+
+        # calculating distance after moving
+        return self.distance(target)
+
+
+if __name__ == '__main__':
+    dt = 0.01
+    target = ManeuveringTarget(500, 1000, 100, 20)
+    missile = ConstrainedMissile(0, 0, 250, 35)
+    t = 0.0
+    hit = False
+    hit_radius = 2.0
+
+    min_d = float("inf")
+
+    while t < 15.00:
+        target.step(dt)
+        d = missile.step(target, dt)
+        if d < min_d:
+            min_d = d
+        if d < hit_radius:
+            hit = True
+            print(f"Target got intercepted in {t//dt} steps")
+            break
+        print(f"distance: {d:.4f}  |  tick: {t:.2f}  |  missile angle: {math.degrees(missile.heading_deg):.2f}")
+        t += dt
+    
+    if not hit:
+        print(f"target was not intercepted and the closest distance was {min_d:.4f}")
